f4d774ef8145238be9cd1a300b026e242a86f5cc
DrMarkEy <maggi321@injc.net>
2020-03-17 21:14:31 +0100

    test

---
+++ app/dist2/precache-manifest.ff30f943d7a5161a5afe517e73a8b5df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c3be112b2fc771733e",
+    "url": "/css/app.e1844cc6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b205e05c4ed21f8cfa1dbb73ea0e0401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13c3be112b2fc771733e",
+    "url": "/js/app.1030c5ba.js"
+  },
+  {
+    "revision": "722b9f10859cca8026d4",
+    "url": "/js/chunk-vendors.97a927d1.js"
+  },
+  {
+    "revision": "aa299d36fbbbe5de74ba2941ef8bfbef",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
