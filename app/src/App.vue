93ffd9a1cf58b1c3bb3c3e93451ea6e3a311ceaf
DrMarkEy <maggi321@injc.net>
2020-03-22 00:02:26 +0100

    Timer wird jetzt übertragen

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,37 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-	
-	<button @click="test">TEST</button>
+  <div id="app">    
+  {{state.timeRemaining}}
+	<button @click="test">Connect!</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+//----- STATUS ------
+// Time Remaining
+// #Strikes
+// Serial #
+// List of Modules
+// Solved Modules
+
+//------ CONFIGURATION ------
+// Time
+// Tick-Sound
+
+//------> Einzelne Module
+
+
+import uuids from './uuids';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  
+  data: function() {
+	return {
+	  state: {
+	    state: -1, //-1: Not connected, 0: inactive, 1: running, 2: paused, 3: solved, 4: exploded
+	    timeRemaining: -1,	  
+	    strikes: 0,
+	    battery: -1,
+	    charging: 0,
+	    moduleCount: 0,
+	    solvedCount: 0,
+		serial: '',
+	  },
+	  
+	  configuration: {
+	    timeTotal: 0,
+		tickSound: undefined
+	  }
+	}
   },
   
   methods: {
 	test: async function() {
-	  // Step 1: Scan for a device with 0xffe5 service
+	  var thi = this;
+	
+	  // Step 1: Scan for a device with the general service
 	  var device = await navigator.bluetooth.requestDevice({
-		filters: [{ services: ["d9696362-1986-4ffd-8be4-ce7a8acada42"] }]
+		filters: [{ services: [uuids.services.state] }]
 	  });
 	 
 	  var server = await device.gatt.connect();
-	  var service = await server.getPrimaryService(0xffe5);
+	  var service = await server.getPrimaryService(uuids.services.state);
 	  
-	  console.log('service', service);
+	  var characteristic = await service.getCharacteristic(uuids.characteristics.state.state);
+	  characteristic.startNotifications();
+	  characteristic.addEventListener('characteristicvaluechanged', function(event) {
+		thi.state.timeRemaining = event.target.value.getUint8(0)+256*event.target.value.getUint8(1);
+	  });	  
+	  
+	  var val = await characteristic.readValue();
+	  
+	  
+	  let decoder = new TextDecoder('utf-8');
+      console.log('> Characteristic User Description: ' + decoder.decode(val));
+	  
+	  
+	  
+	  //SET LED
+	  /*var LEDcharacteristic = await service.getCharacteristic("7961cceb-a950-4dc9-8f38-d8ed8931fce6");	  
+	  
+	  var str = "1";
+	  let buffer = new ArrayBuffer(1);
+      let dataView = new DataView(buffer);      
+      dataView.setUint8(0, str.charAt(0).charCodeAt());      
+	  
+	  var val = await LEDcharacteristic.writeValue(buffer);*/
+	  //console.log('value', val);
 	  /*
-	  .then(function(service) {
-		// Step 4: get the Characteristic
-		return service.getCharacteristic(0xffe9);
-	  })
 	  .then(function(characteristic) {
 		// Step 5: Write to the characteristic
 		var data = new Uint8Array([0xbb, 0x25, 0x05, 0x44]);
---
+++ app/src/uuids.js
@@ -0,0 +1,20 @@
+var uuids = {
+	services: {
+		state: "d9696362-1986-4ffd-8be4-ce7a8acada42"		
+	},
+	
+	characteristics: {
+		state: {
+			state: "5ef9c1c6-2bec-4692-838b-9572b88bd41e",
+			timeRemaining: "2e821797-41d7-4a4f-8ca2-cbfbe1a329a9",
+			strikes: "dfdcb5e3-de11-46e2-aef3-fb969e3bc6e6",
+			battery: "619b947b-4095-498d-b91a-83a8febb8e0e",
+			charging: "e8ebed5a-48b7-4598-a0b9-76e2fa09b235",
+			moduleCount: "8c2668c5-de50-4f05-848c-b5aafce02283",
+			solvedCount: "bf624b71-ce58-4851-85e6-f0895de68041",
+			serial: "9be5491f-198c-4d6b-9f97-7634d63fd30f",
+		},
+	}
+};
+
+export default uuids;
